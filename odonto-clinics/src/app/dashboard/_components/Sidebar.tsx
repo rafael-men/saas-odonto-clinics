b30e7f5523d51f5f0d3f21eeddfc722cd516f6f4
rafael-men <rafaelgoncalves1822@gmail.com>
2025-12-04 17:35:11 -0300

    sidebar

--- odonto-clinics/src/app/dashboard/_components/Sidebar.tsx
+++ odonto-clinics/src/app/dashboard/_components/Sidebar.tsx
@@ -11,8 +11,11 @@ import {
   SheetTrigger 
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
-import { CalendarCheck2, Folder, List, Settings } from "lucide-react";
+import { CalendarCheck2, ChevronLast, ChevronLeft, ChevronRight, Folder, List, PersonStanding, Settings } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from '../../../../public/icon.png';
+import { Collapsible, CollapsibleContent } from "@radix-ui/react-collapsible";
 
 export function Sidebar({children} : {children : React.ReactNode}) {
     const pathname = usePathname()
@@ -21,6 +24,79 @@ export function Sidebar({children} : {children : React.ReactNode}) {
 
     return (
         <div className="flex min-h-screen w-full">
+            <aside className={clsx("flex flex-col border-r bg-background transition-all duration-300 h-full p-4",{
+                'w-20':Collapsed,
+                'w-64':!Collapsed,
+                'hidden md:flex md:fixed':true
+            })}>
+
+            <div className="mb-6 mt-4" style={{ display: 'flex', alignItems: 'center' }}>
+             {!Collapsed && (
+            <>
+            <Image 
+                src={Logo} 
+                alt="logo" 
+                priority 
+                quality={100} 
+                style={{
+                    width: '50px',
+                    height: '50px',
+                    marginRight: '10px' 
+                }}
+            />
+            <h1 className="font-semibold" style={{ margin: 0 }}>Odonto Clinics</h1>
+            </>
+            )}
+           </div>
+
+
+            <Button className="bg-gray-100 w-full hover:bg-gray-200 text-zinc-900 self-end mb-2"
+                onClick={() => setCollapsed(!Collapsed)}>
+                {!Collapsed ? <ChevronLeft className="h-12 w-12"/> : <ChevronRight className="w-12 h-12"/>}
+            </Button>
+
+            {Collapsed && (
+                 <nav className="flex flex-col gap-1 overflow-hidden">
+                <SidebarNav href="/dashboard" label="Agendamentos" pathname={pathname} collapsed={Collapsed} 
+                icon={<CalendarCheck2 className="w-6 h-6"/>}/>
+
+                <SidebarNav href="/dashboard/services" label="Serviços" pathname={pathname} collapsed={Collapsed} 
+                icon={<Settings className="w-6 h-6"/>}/>
+
+                <SidebarNav href="/dashboard/profile" label="Perfil" pathname={pathname} collapsed={Collapsed} 
+                icon={<PersonStanding className="w-6 h-6"/>}/>
+                 </nav>
+            )}
+
+
+            <Collapsible open={!Collapsed}>
+            <CollapsibleContent>
+            <nav className="flex flex-col gap-1 overflow-hidden">
+
+                <span className="text-sm text-gray-800 font-medium ">
+                    Painel
+                </span>
+
+                <SidebarNav href="/dashboard" label="Agendamentos" pathname={pathname} collapsed={Collapsed} 
+                icon={<CalendarCheck2 className="w-6 h-6"/>}/>
+
+                <SidebarNav href="/dashboard/services" label="Serviços" pathname={pathname} collapsed={Collapsed} 
+                icon={<Settings className="w-6 h-6"/>}/>
+
+                <span className="text-sm text-gray-800 font-medium ">
+                    Configurações
+                </span>
+
+                <SidebarNav href="/dashboard/profile" label="Perfil" pathname={pathname} collapsed={Collapsed} 
+                icon={<PersonStanding className="w-6 h-6"/>}/>
+                
+
+            </nav>
+            </CollapsibleContent>
+            </Collapsible>
+            </aside>
+
+
             <div className={clsx("flex flex-1 flex-col transition-all duration-300",{
                 'md:ml-20' : Collapsed, 
                 'md:ml-64' : !Collapsed
@@ -45,7 +121,7 @@ export function Sidebar({children} : {children : React.ReactNode}) {
                         <SheetTitle className="text-center pt-5">Odonto Clinic</SheetTitle>
 
                         <nav className="grid gap-2 text-base pt-5">
-                            <SidebarNav href="/dashboard" label="dashboard" pathname={pathname} collapsed={Collapsed} 
+                            <SidebarNav href="/dashboard" label="Agendamentos" pathname={pathname} collapsed={Collapsed} 
                             icon={<CalendarCheck2 className="w-6 h-6"/>}/>
 
                             <SidebarNav href="/dashboard/services" label="Serviços" pathname={pathname} collapsed={Collapsed} 
---
+++ odonto-clinics/src/lib/prisma.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+let prisma: PrismaClient;
+const adapter = new PrismaPg({connectionString})
+
+if(process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient({
+        adapter
+    })
+} 
+else {
+    let globalPrisma = global as typeof globalThis & {
+        prisma: PrismaClient
+    }
+
+    if(!globalPrisma.prisma) {
+        globalPrisma.prisma = new PrismaClient({adapter});
+
+    prisma = globalPrisma.prisma
+}}
+
+export default prisma;
